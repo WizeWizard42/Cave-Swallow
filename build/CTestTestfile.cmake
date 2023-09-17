a7589cab7db0ccc8a4cf0b82d028f0c5f6935059
Merlin Mackley <merlin.newcomb@gmail.com>
2023-09-17 13:51:21 -0700

    Setup CMake, initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/merli/Documents/Projects/Cave-Swallow
+# Build directory: C:/Users/merli/Documents/Projects/Cave-Swallow/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
