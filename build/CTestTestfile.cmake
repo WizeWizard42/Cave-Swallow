a6d3a9e710061329606f96770046f0b1c2c03687
Merlin Newcomb <51838647+WizeWizard42@users.noreply.github.com>
2023-12-08 16:04:49 -0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/merli/Documents/Projects/Cave-Swallow
-# Build directory: C:/Users/merli/Documents/Projects/Cave-Swallow/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
